9a9c40582bd730901c91ebc89837f7e4cfc853f3
Serge Rodovnichenko <sergerod@gmail.com>
2015-07-28 23:18:02 +0300

    Autogenerated

---
+++ lib/config/plugin.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'name' => 'Deliverylist',
+  'icon' => 'img/deliverylist.gif',
+  'version' => '0.1',
+  'vendor' => '670917',
+  'handlers' => 
+  array (
+  ),
+);
---
+++ lib/shopDeliverylistPlugin.class.php
@@ -0,0 +1,6 @@
+<?php
+
+class shopDeliverylistPlugin extends shopPlugin
+{
+
+}
