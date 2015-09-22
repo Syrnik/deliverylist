6a32d7e4fe3da71c3b6100ed681f9bad484d6bb3
Serge Rodovnichenko <sergerod@gmail.com>
2015-09-23 01:47:33 +0300

    Moved action into Actions cntrlr

--- lib/actions/shopDeliverylistPluginPrintform.actions.php
+++ lib/actions/shopDeliverylistPluginPrintform.actions.php
@@ -12,17 +12,36 @@
 /**
  * Printform
  */
-class shopDeliverylistPluginPrintformDisplayAction extends waViewAction
+class shopDeliverylistPluginPrintformActions extends waViewActions
 {
-    public function execute()
+    private $orders = array();
+
+    protected function preExecute()
     {
         $order_ids = waRequest::post('ids', null, waRequest::TYPE_ARRAY_INT);
         $collection = new shopOrdersCollection($order_ids);
-        $orders = $collection->getOrders('*,params,items,contact');
-        shopHelper::workupOrders($orders);
+        $this->orders = $collection->getOrders('*,params,items,contact');
+        shopHelper::workupOrders($this->orders);
+        $this->orders = $this->addWeight($this->orders);
+        parent::preExecute();
+    }
 
+    public function displayAction()
+    {
+        $this->view->assign('orders', $this->orders);
+    }
+
+    /**
+     * Добавляет вес к товарам в заказе
+     *
+     * @param array $orders
+     * @return array
+     * @throws waException
+     */
+    private function addWeight($orders)
+    {
         $weightDimension = shopDimension::getInstance()->getDimension('weight');
-        $multiplier = null;
+        $multiplier = 1;
         if ($weightDimension['base_unit'] != 'kg') {
             $multiplier = $weightDimension['units']['kg']['multiplier'];
         }
@@ -44,20 +63,21 @@ class shopDeliverylistPluginPrintformDisplayAction extends waViewAction
 
                 foreach ($val['items'] as &$item) {
                     if (isset($weights['skus'][$item['sku_id']])) {
-                        $item['weight'] = $weights['skus'][$item['sku_id']];
+                        $item['weight'] = $weights['skus'][$item['sku_id']] * $multiplier;
                     } else {
-                        $item['weight'] = isset($weights[$item['product_id']]) ? $weights[$item['product_id']] : 0;
+                        $item['weight'] = isset($weights[$item['product_id']]) ?
+                            $weights[$item['product_id']] * $multiplier : 0;
                     }
                 }
-
             } else {
                 foreach ($val['items'] as &$item) {
                     $item['weight'] = 0;
                 }
             }
+            unset($item);
             $orders[$key] = $val;
         }
 
-        $this->view->assign(compact('orders'));
+        return $orders;
     }
 }
