f8cc741c5d4c891adbcef5f49b1a6229ec8e1d43
Serge Rodovnichenko <sergerod@gmail.com>
2015-09-23 02:08:27 +0300

    Requirements

---
+++ lib/config/requirements.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * List of selected orders for delivery division, couriers e t.c
+ *
+ * @author Serge Rodovnichenko <serge@syrnik.com>
+ * @version 1.0.0
+ * @copyright Serge Rodovnichenko, 2015
+ * @license http://www.webasyst.com/terms/#eula Webasyst
+ * @package deliverylist
+ */
+return array(
+    'php.zip' => array(
+        'description' => 'Расширение zip необходимо для создания файлов формата MS Excel 2007 (xlsx)',
+        'strict'      => false
+    )
+);
